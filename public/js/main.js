2a543d997c1efb43fac22eb2b647d1d69f012b5e
Beny <b.ntambwe97@gmail.com>
2021-01-11 12:11:56 +0100

    fin exo

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+// EXO 1
+// let quest = prompt('Choisissez un chiffre')
+// let compt = 0
+// while (compt <= quest) {
+//     console.log(compt);
+//     nbr++
+// }
+
+// EXO 2
+let compt = 0
+let a = 3
+let b = 10
+let c = 25
+while (compt <= a) {
+    console.log(compt);
+    compt++
+}
+
+while (compt <= b) {
+    console.log(compt);
+    compt++
+}
+while (compt <= c) {
+    console.log(compt);
+    compt++
+}
